b4f938fb7d0f70c7eb19dd87dcba605e2cdad25a
Knut Jelitto <knut.jelitto@live.com>
2018-05-03 07:25:06 +0200

    changes

--- bin/tools/functions.sh
+++ bin/tools/functions.sh
@@ -9,9 +9,9 @@ function get-pkg-script {
 function unmount-all {
     local lfs=$1
     while mount | grep -q "/${lfs}/"; do
-        mount | grep "/${lfs}/" | cut -f3 -d" " | sort | head --lines=1 | while read mountpoint; do
-            echo "umount ${mountpoint}"
-            umount --recursive ${mountpoint}
+        mount | grep "/${lfs}/" | cut -f3 -d" " | sort | while read mountpoint; do
+            #echo "umount ${mountpoint}"
+            mountpoint --quiet ${mountpoint} &>/dev/null && umount --recursive ${mountpoint} &>/dev/null
         done
     done
 }
--- bin/tools/init.sh
+++ bin/tools/init.sh
@@ -1,6 +1,8 @@
 scriptlocation=$(dirname $0)
+etclocation=${scriptlocation}/../etc
 
 source ${scriptlocation}/tools/functions.sh
 
+LifoTop=~/lifo
 LfsTop=~/lfs
 
