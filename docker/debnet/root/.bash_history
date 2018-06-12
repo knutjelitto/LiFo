27cd91aed136a697465bb245091e6da16281cb4a
Jelitto, Knut <knut.jelitto@sartorius.com>
2018-06-12 16:10:09 +0200

    changes

--- docker/debnet/root/.bash_history
+++ docker/debnet/root/.bash_history
@@ -7,3 +7,30 @@ pwsh -o XML -C Get-Date
 pwsh -Help
 pwsh -Help | less
 pwsh
+./configure
+OAOR
+[<0;50;42M
+./BinUtils
+pwsh -Help | less
+pwsh -help
+pwsh | less
+pwsh -help | less
+pwsh -version
+pwsh -w Minimized
+pwsh -NoProfile
+apt-get update
+apt-get install mc
+mc
+pwsh
+mc
+apt-get update
+apt-get install man-db
+apt-get install manpages
+man
+man2 chroot
+man 2 chroot
+pwsh
+wnv
+env
+lll
+b
---
+++ pwsh/MyModule/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+
+namespace MyModule
+{
+    [Cmdlet("Change", "Root")]
+    public class WriteTimestampedMessageCommand : PSCmdlet
+    {
+        [Parameter(Position=0,Mandatory=true)]
+        public string Path { get; set; } = string.Empty;
+        
+        [DllImport("libc.so.6")]
+        private static extern int getpid();
+
+        [DllImport("libc.so.6")]
+        private static extern int chroot(string path);
+
+        protected override void EndProcessing()
+        {
+            var pid = getpid();
+            string timestamp = DateTime.Now.ToString("u");
+            this.WriteObject($"[{timestamp}] - {this.Path} - {pid}");
+            var result = chroot("/Pogo");
+            this.WriteObject($"[{result}]");
+            base.EndProcessing();
+        }
+    }
+}
