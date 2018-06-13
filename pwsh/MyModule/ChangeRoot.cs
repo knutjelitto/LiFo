81a6fb349a06ae1111845a827fc91fe067076b1a
Jelitto, Knut <knut.jelitto@sartorius.com>
2018-06-13 16:11:33 +0200

    changes

--- pwsh/MyModule/ChangeRoot.cs
+++ pwsh/MyModule/ChangeRoot.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Management.Automation;
 using System.Runtime.InteropServices;
+using Mono.Unix.Native;
 
 namespace MyModule
 {
-    [Cmdlet("Change", "Root")]
-    public class WriteTimestampedMessageCommand : PSCmdlet
+    [Cmdlet("Set", "Root")]
+    public class ChangeRoot : PSCmdlet
     {
         [Parameter(Position=0,Mandatory=true)]
         public string Path { get; set; } = string.Empty;
@@ -15,13 +16,13 @@ namespace MyModule
 
         [DllImport("libc.so.6")]
         private static extern int chroot(string path);
-
         protected override void EndProcessing()
         {
+            
             var pid = getpid();
             string timestamp = DateTime.Now.ToString("u");
             this.WriteObject($"[{timestamp}] - {this.Path} - {pid}");
-            var result = chroot("/Pogo");
+            var result = Syscall.chroot("/Pogo");
             this.WriteObject($"[{result}]");
             base.EndProcessing();
         }
